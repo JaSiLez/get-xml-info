62cad1fafd56b010264a1c92b42ff8593835e3cd
alvarezfr <80817164+alvarezfr@users.noreply.github.com>
2022-10-06 17:13:08 +0200

    update eslint

--- index.js
+++ index.js
@@ -1,59 +1,58 @@
-const core = require('@actions/core');
-const fs = require('fs');
+const core = require('@actions/core')
+const fs = require('fs')
 
 try {
   console.log('Welcome to Get-XML-Version.')
-    
-  var argv = require('minimist')(process.argv.slice(2));
-  var xmlFile = (typeof argv.f !== 'undefined') ? argv.f : process.env.GITHUB_WORKSPACE+'/'+core.getInput('xml-file', { required: true });
-  var xpathToSearch = (typeof argv.p !== 'undefined') ? argv.p : core.getInput('xpath', { required: true });
-  var debug = (typeof argv.d !== 'undefined') ? true : false;
 
-  console.log(`File to read: ${xmlFile}`);
-  console.log(`XPath: ${xpathToSearch}`);
+  const argv = require('minimist')(process.argv.slice(2))
+  const xmlFile = (typeof argv.f !== 'undefined') ? argv.f : process.env.GITHUB_WORKSPACE + '/' + core.getInput('xml-file', { required: true })
+  const xpathToSearch = (typeof argv.p !== 'undefined') ? argv.p : core.getInput('xpath', { required: true })
+  const debug = typeof argv.d !== 'undefined'
 
-  var xpath = require('xpath'), dom = require('xmldom').DOMParser
- 
-  fs.readFile(xmlFile, 'utf8', function read(err, data) {
+  console.log(`File to read: ${xmlFile}`)
+  console.log(`XPath: ${xpathToSearch}`)
+
+  const xpath = require('xpath')
+  const Dom = require('xmldom').DOMParser
+
+  fs.readFile(xmlFile, 'utf8', function read (err, data) {
     if (err) {
-      core.setFailed(err.message);
-    }
-    else {
-      console.log('File was read successfully. Proceeding to parse DOM.');
-      
-      var doc = new dom().parseFromString(data);
+      core.setFailed(err.message)
+    } else {
+      console.log('File was read successfully. Proceeding to parse DOM.')
+
+      const doc = new Dom().parseFromString(data)
       if (debug) {
-        console.log('Debug output: Document.');
-        console.log(doc);
+        console.log('Debug output: Document.')
+        console.log(doc)
       }
 
-      var nodes = xpath.select(xpathToSearch, doc);
+      const nodes = xpath.select(xpathToSearch, doc)
       if (debug) {
-        console.log('Debug output: Nodes.');
-        console.log(nodes);
+        console.log('Debug output: Nodes.')
+        console.log(nodes)
       }
 
-      console.log(`Found ${nodes.length} nodes.`);
+      console.log(`Found ${nodes.length} nodes.`)
 
       if (nodes.length) {
-        var output = [];
-        for (var i = 0; i < nodes.length; i++) {
-          var node = nodes[i];
-          var firstChild = node.firstChild;
+        const output = []
+        for (let i = 0; i < nodes.length; i++) {
+          const node = nodes[i]
+          const firstChild = node.firstChild
           if (firstChild) {
-            output.push(firstChild.data);
+            output.push(firstChild.data)
           } else {
-            output.push(node.value);
+            output.push(node.value)
           }
         }
-        core.setOutput('info', output);
-        console.log(`Output: ${output}`);
-      }
-      else {
-        core.setFailed('Your xpath did not return any nodes.');
+        core.setOutput('info', output)
+        console.log(`Output: ${output}`)
+      } else {
+        core.setFailed('Your xpath did not return any nodes.')
       }
     }
-  });
+  })
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(error.message)
 }
